63b910ce7ef579a6ac41573dcbc243a1d2786993
alex <alex@gmail.com>
2025-09-08 21:44:08 -0700

    add docker_scripts

---
+++ docker_scripts/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+echo "📦 Applying database migrations..."
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+echo "🧱 Collecting static files..."
+python manage.py collectstatic --no-input
+
+# echo "👤 Creating superuser if it doesn't exist..."
+# python manage.py create_custom_superuser
+
+exec "$@"
