a4661a1611cd85fbd071ebddf01ef49f44504688
alex <alex@gmail.com>
2025-09-16 00:02:07 -0700

    add test_urls in accounts app

---
+++ accounts/tests/test_urls.py
@@ -0,0 +1,31 @@
+import uuid
+from django.urls import reverse
+
+def test_register_url():
+    url=reverse('accounts:register')
+    assert url == '/register/'
+
+def test_login_url():
+    url=reverse('accounts:login')
+    assert url == '/register/'
+
+def test_logout_url():
+    url=reverse('accounts:logout')
+    assert url == '/logout/'
+
+def test_password_reset_request_url():
+    url=reverse('accounts:password-reset-request')
+    assert url == '/password_reset_request/'
+
+def test_password_reset_confirm_url():
+    token = uuid.uuid4()
+    url=reverse('accounts:password-reset-confirm', args=[token])
+    assert url == f'/password_reset_confirm/{token}/'
+
+def test_profile_url():
+    url=reverse('accounts:profile')
+    assert url == '/profile/'
+
+def test_profile_update_url():
+    url=reverse('accounts:profile-update')
+    assert url == '/profile/update/'
