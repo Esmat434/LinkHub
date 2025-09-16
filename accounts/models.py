2500e4bdaedbef8baf7d6b37bd08e58a902cbd82
alex <alex@gmail.com>
2025-09-15 21:35:31 -0700

    fix models in accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -11,22 +11,14 @@ class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     slug = models.SlugField(unique=True)
     avatar = models.ImageField(upload_to='users/', blank=True, null=True)
-    birth_date = models.DateField(blank=True, null=True)
-    email_verified = models.BooleanField(default=False)
 
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.username)
         return super().save(*args,**kwargs)
 
-class Tokens(models.Model):
-    class Token_Type(models.TextChoices):
-        email_verified='email_verified','Email Verified'
-        change_password='change_password','Change Password'
-        forgot_password='forgot_password','Forgot Password'
-
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-    type = models.CharField(max_length=100, choices=Token_Type, default=Token_Type.email_verified)
+class ForgotPassword(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='forgot_password')
     token = models.UUIDField(default=uuid4)
     created_at = models.DateField(auto_now_add=True)
 
