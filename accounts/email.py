1b45d3272a2cfc54023f24b3f03f07f4bc0ff0f0
alex <alex@gmail.com>
2025-09-20 22:48:29 -0700

    fix email in accounts app

--- accounts/email.py
+++ accounts/email.py
@@ -1,9 +1,11 @@
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.conf import settings
+from celery import shared_task
 
+@shared_task
 def send_verification_email_async(to,subject,username,link):
-    html_content=render_to_string('accounts/email.html',{'username':username,'link':link})
+    html_content=render_to_string('accounts/password_reset_email.html',{'username':username,'link':link})
     email=EmailMessage(subject,html_content,settings.EMAIL_HOST_USER,[to])
     email.content_subtype='html'
 
