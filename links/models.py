802fd29e2ddded6ce8ab725f615fd4cbbd11a389
alex <alex@gmail.com>
2025-09-21 22:57:03 -0700

    created link model in lins app

--- links/models.py
+++ links/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Link(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='link', verbose_name='link')
+    title = models.CharField(max_length=150, unique=True,verbose_name='title')
+    url = models.URLField(verbose_name='url')
+    icon = models.ImageField(upload_to='links/icons')
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Link'
+        verbose_name_plural = 'Links'
