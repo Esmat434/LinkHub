667bc87a44620f01410ae271443f2c44150e45ec
alex <alex@gmail.com>
2025-09-22 01:39:51 -0700

    fix models in links app

--- links/models.py
+++ links/models.py
@@ -4,12 +4,16 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Link(models.Model):
+    class LinkStatus(models.TextChoices):
+        enable = 'Enable'
+        disable = 'Disable'
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='link', verbose_name='link')
-    title = models.CharField(max_length=150, unique=True,verbose_name='title')
+    title = models.CharField(max_length=150,verbose_name='title')
     url = models.URLField(verbose_name='url')
-    icon = models.ImageField(upload_to='links/icons')
-    status = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    icon = models.ImageField(upload_to='links/icons', verbose_name='icon')
+    status = models.BooleanField(choices=LinkStatus, default=LinkStatus.enable, verbose_name='status')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='created date')
 
     def __str__(self):
         return self.title
