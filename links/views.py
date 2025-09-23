9df8337fe0406397aec7abe9f044dae2cfd0d421
alex <alex@gmail.com>
2025-09-23 03:24:11 -0700

    add migrations in links app

--- links/views.py
+++ links/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from accounts.mixins import (
+    LoginRequiredMixin
+)
+
+from .models import (
+    Link
+)
+
