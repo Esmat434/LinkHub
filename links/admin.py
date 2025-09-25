afb8090ea709d9b2745db5cdf194a1ae0f356446
alex <alex@gmail.com>
2025-09-25 00:49:08 -0700

    fix admin in links app

--- links/admin.py
+++ links/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
 from .models import (
-    Link
+    Link,PageView
 )
 
 @admin.register(Link)
 class LinkAdmin(admin.ModelAdmin):
     list_display = ['user','title','url','status','created_at']
+
+@admin.register(PageView)
+class PageViewAdmin(admin.ModelAdmin):
+    list_display = ['user','path','ip_address','user_agent','created_at']
