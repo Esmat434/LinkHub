7ccd96868f6a5a9c43abce782e6c978ec59e5ea3
alex <alex@gmail.com>
2025-09-25 00:49:24 -0700

    fix models in links app

--- links/models.py
+++ links/models.py
@@ -8,11 +8,11 @@ class Link(models.Model):
         enable = 'Enable'
         disable = 'Disable'
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='link', verbose_name='link')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='links', verbose_name='link')
     title = models.CharField(max_length=150,verbose_name='title')
     url = models.URLField(verbose_name='url')
     icon = models.ImageField(upload_to='links/icons', verbose_name='icon')
-    status = models.BooleanField(choices=LinkStatus, default=LinkStatus.enable, verbose_name='status')
+    status = models.CharField(max_length=10,choices=LinkStatus, default=LinkStatus.enable, verbose_name='status')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='created date')
 
     def __str__(self):
@@ -21,3 +21,17 @@ class Link(models.Model):
     class Meta:
         verbose_name = 'Link'
         verbose_name_plural = 'Links'
+
+class PageView(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='pageview', verbose_name='User')
+    path = models.CharField(max_length=255, verbose_name='Path')
+    ip_address = models.GenericIPAddressField(null=True, blank=True, verbose_name='IP_Address')
+    user_agent = models.TextField(null=True, blank=True, verbose_name='User Agent')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created Date')
+
+    def __str__(self):
+        return f"IP: {self.ip_address} Related to: {self.user.username if self.user else 'Anonymous'}"
+    
+    class Meta:
+        verbose_name = 'Page View'
+        verbose_name_plural = 'Page Views'
