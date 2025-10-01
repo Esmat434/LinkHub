d5bbb57f873d93622e1f1241fe6a80fbd144e6bd
alex <alex@gmail.com>
2025-09-30 23:41:49 -0700

    fix urls in links app

--- links/urls.py
+++ links/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
 
 from .views import (
-    MyPageView,DashboardView,LinkListView,LinkCreateView,LinkUpdateView
+    MyPageView,DashboardView,LinkListView,LinkDetailView,LinkCreateView,LinkUpdateView,
+    LinkDeleteView
 )
 
 app_name = "links"
 
 urlpatterns = [
-    path('my_page/<slug:slug>/', MyPageView.as_view(), name='my_page'),
-    path('dashboard/', DashboardView.as_view(), name='dashboard'),
-    path('link/list/', LinkListView.as_view(), name='link_list'),
-    path('link/create/', LinkCreateView.as_view(), name='link_create'),
-    path('link/update/<int:pk>/', LinkUpdateView.as_view(), name="link_update")
+    path('', DashboardView.as_view(), name='dashboard'),
+    path('my_page/<slug:slug>/', MyPageView.as_view(), name='my-page'),
+    path('link/list/', LinkListView.as_view(), name='link-list'),
+    path('link/detail/<int:pk>/', LinkDetailView.as_view(), name='link-detail'),
+    path('link/create/', LinkCreateView.as_view(), name='link-create'),
+    path('link/update/<int:pk>/', LinkUpdateView.as_view(), name="link-update"),
+    path('link/delete/<int:pk>/', LinkDeleteView.as_view(), name='link-delete')
 ]
